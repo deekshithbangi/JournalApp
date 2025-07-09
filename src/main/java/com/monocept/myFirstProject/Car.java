0999e365af18f408123a435cbc82a89518e12c81
deekshithbangi <deekshithbangi1@gmail.com>
2025-07-09 11:32:48 +0530

    myFirstProject

---
+++ src/main/java/com/monocept/myFirstProject/Car.java
@@ -0,0 +1,19 @@
+package com.monocept.myFirstProject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Car {
+
+    @Autowired
+    private Dog dog;        // dependency injection
+
+//  Dog dog  = new Dog();
+
+    @GetMapping("/ok")
+    public String ok() {
+        return dog.fun();
+    }
+}
---
+++ src/main/java/com/monocept/myFirstProject/Dog.java
@@ -0,0 +1,11 @@
+package com.monocept.myFirstProject;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog {
+
+    public String fun() {
+        return "Hello From Something package";
+    }
+}
